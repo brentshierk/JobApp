15b49d42a0080f399e05524d74f8506780c2be7b
bshierk1996 <34776645+bshierk1996@users.noreply.github.com>
2019-01-08 21:53:11 -0500

    compleate job app

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from time import sleep
 import urllib.request as u
 import smtplib
 import csv
@@ -6,86 +8,86 @@ import csv
 # Scrape the main search results page
 # Here we're getting the link to the details page
 # for each search result
-
-
 def scrape_listing_results(url):
-  # Open the link to the page using URLLib2
-  response = u.urlopen(url).read()
-  # Make the page parse-able using BeautifulSoup
-  soup = bs(response, features="lxml")
-  # Find the link to the result's main page using BeautifulSoup
-  results = soup.find_all('a', class_='result-title')
+	# Open the link to the page using URLLib2
+	response = u.urlopen(url).read()
+	# Make the page parse-able using BeautifulSoup
+	soup = bs(response, "html.parser")
+	# Find the link to the result's main page using BeautifulSoup
+	results = soup.find_all('a', class_='result-title')
 
-
-  # Create a new array with the links to detail pages
-  # for each result on the search results page
-  result_list = [listing['href'] for listing in results]
-  return result_list
+	# Create a new array with the links to detail pages
+	# for each result on the search results page
+	result_list = [listing['href'] for listing in results]
+	return result_list
 
 # For each link on the search results page,
 # Open the details page and grab the email
-
-
 def gather_listing_data(urlList):
-  # Empty array that will hold the contact data
-  # This essentially replaces the CSV
-  contact = []
-  for url in urlList:
-    contactInfo = {}
-    # Open the link to the page using URLLib2
-    response = u.urlopen(url)
-    # Make the page parse-able using BeautifulSoup
-    soup = bs(response.read())
+	# Empty array that will hold the contact data
+	# This essentially replaces the CSV
+	contact = []
+	for url in urlList:
+		contactInfo = {}
+		# Open the link to the page using URLLib2
+		response = u.urlopen(url)
 
-    # Find the title and email on the listing page using BeautifulSoup
-    listingtitle = soup.find('span', _id="titletextonly")
-    #emailaddr = soup.find('div', class_="lbcontent")
+		# Make the page parse-able using BeautifulSoup
+		soup = bs(response.read(), "html.parser")
 
-    # Create a new object using the data we got from BeauitfulSoup
-    contactInfo['address'] = emailaddr.get_text()
-    contactInfo['title'] = listingtitle.get_text()
-    contactInfo['message'] = f'email address:{contactinfo['address']} Hello I was wondering if the job was still avaible, if so my resume is attached below'
+		# Find the title and email on the listing page using BeautifulSoup
+		listing_title = soup.find('span', id="titletextonly")
 
-    contact.append(contactInfo)
-    return contact
-  print(contact)
+		# Create a new object using the data we got from BeauitfulSoup
+		contactInfo['address'] = get_email(url)
+		contactInfo['title'] = listing_title.contents[0]
+		contactInfo['message'] = 'Hello I was wondering if the job was still avaible, if so my resume is attached below'
+
+		contact.append(contactInfo)
+		print(contact)
+	return contact
+
+# Selenium stuff
+def get_email(url):
+	driver = webdriver.Chrome()
+	driver.get(url)
+	
+	cta = driver.find_element_by_class_name('reply_button')
+	cta.click()
+	sleep(10)
+
+	return driver.find_element_by_class_name('anonemail').text
+
+
 # initial login
-
-
 def initial_login():
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.connect('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-    server.login('pythontest667@gmail.com', 'roxxy123')
-    return server
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.connect('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	server.login('pythontest667@gmail.com', 'roxxy123')
+	return server
 
 # Take an entry (email address and message) and send it
-
-
 def send_mail(entry, server):
-    # server.sendmail('pythontest667@gmail.com', entry['address'], entry['message'])
-    server.sendmail('pythontest667@gmail.com',
-                    'pythontest667@gmail.com', entry['message'])
-    print('message sent')
+	# server.sendmail('pythontest667@gmail.com', entry['address'], entry['message'])
+	server.sendmail('pythontest667@gmail.com', 'pythontest667@gmail.com', entry['message'])
+	print('message sent')
 
 # Program is initialized here
-
-
 def main():
-  # Scrape
-  primary_link = 'https://newyork.craigslist.org/search/jjj?query=barista&sort=rel'
-  link_list = scrape_listing_results(primary_link)
-  details_list = gather_listing_data(link_list)
+	# Scrape
+	primary_link = 'https://newyork.craigslist.org/search/jjj?query=barista&sort=rel'
+	link_list = scrape_listing_results(primary_link)
+	# print(link_list)
+	details_list = gather_listing_data(link_list)
+	# Log in once (rather than each time in send_mail)
+	server = initial_login()
 
-  # Log in once (rather than each time in send_mail)
-  server = initial_login()
-
-  # For each entry in the JSON file, send an email
-  for entry in details_list:
-      send_mail(entry, server)
-
+	# For each entry in the JSON file, send an email
+	for entry in details_list:
+		send_mail(entry, server)
 
 # Run the program
 main()
